441e97c2a2cf9e9315e533fa59000a90cecd8fb0
potatoxchips2 <123182275+potatoxchips2@users.noreply.github.com>
2026-08-14 21:49:41 -0400

    updates :P

--- app.py
+++ app.py
@@ -1,59 +1,93 @@
 import sqlite3 #stores data in a local database
 import pandas as pd # handles returned data
 import streamlit as st # creates a web app
+st.set_page_config(
+    page_title="Budget Tracker",
+    layout="wide"
+)
 
-conn = sqlite3.connect('budget.db') # connects to a database file called budget.db
 
+conn = sqlite3.connect('budget.db') # connects to the budget database
 
+#title and subtitle for the web app
 st.title("Budget Transactions")
-st.subheader("Transactions")
 
-col1, col2 = st.columns(2)
+overview, transactions = st.columns(2)
 
-with col1:
-    status_filter = st.selectbox("Filter by Status", [
-        "All", "Pending", "Posted"])
-    
-if status_filter == "All":
-    query = """
-    SELECT transaction_date, merchant, amount, status
-    FROM transactions;
+with overview:
+    st.subheader("Overview")
+
+    #total amount of pending 
+    pending_query = """
+        SELECT SUM(amount) AS pending_total
+        FROM transactions 
+        WHERE status = 'Pending' AND transaction_type = 'Purchase'
     """
 
-    df = pd.read_sql_query(query, conn)
-    
-if status_filter == "Pending":
-    query = """
-    SELECT transaction_date, merchant, amount, status
-    FROM transactions
-    WHERE status = 'Pending';
-    """
-    df = pd.read_sql_query(query, conn)
+    pending_df = pd.read_sql_query(pending_query, conn) 
+    pending_spending = pending_df["pending_total"].iloc[0] 
 
-if status_filter == "Posted":
-    query = """
-    SELECT transaction_date, merchant, amount, status
-    FROM transactions
-    WHERE status = 'Posted';
+    #total amount of posted
+    posted_query = """
+        SELECT SUM(amount) AS posted_total
+        FROM transactions 
+        WHERE status = 'Posted' AND transaction_type = 'Purchase'
     """
-    df = pd.read_sql_query(query, conn) 
-    
-with col2:
-    order_by = st.selectbox("Order by", [
-        "Oldest to Newest", "Newest to Oldest"])
 
-if order_by == "Oldest to Newest":
-    query = """
-    SELECT transaction_date, merchant, amount, status
-    FROM transactions
-    ORDER BY transaction_date ASC;
-    """
+    posted_df = pd.read_sql_query(posted_query, conn)
+    posted_spending = posted_df["posted_total"].iloc[0]
+
+    #display
+    total, pending, posted = st.columns(3)
+
+    with total:
+        st.metric(
+            label="Total Spending",
+            value=f"${pending_spending + posted_spending:,.2f}"
+        )
+
+    with pending:
+        st.metric(
+            label="Pending Spending",
+            value=f"${pending_spending:,.2f}"
+        )
+
+    with posted:
+        st.metric(
+            label="Posted Spending",
+            value=f"${posted_spending:,.2f}"
+        )
+
+
+
+with transactions:
+    st.subheader("Transactions")
+
+    #display
+    status_filter, order_filter = st.columns(2)
+
+    with status_filter:
+        status_filter = st.selectbox("Filter by Status", [
+            "All", "Pending", "Posted"])
     
-if order_by == "Newest to Oldest":
+    with order_filter:
+        order_by = st.selectbox("Order by", [
+            "Oldest to Newest", "Newest to Oldest"])
+
     query = """
-    SELECT transaction_date, merchant, amount, status
-    FROM transactions
-    ORDER BY transaction_date DESC;
-    """
+        SELECT transaction_date, merchant, amount, status
+        FROM transactions
+    """    
     
-st.dataframe(df)
+    if status_filter == "Pending":
+        query += "WHERE status = 'Pending' "
+    elif status_filter == "Posted":
+        query += "WHERE status = 'Posted' "
+    
+    if order_by == "Oldest to Newest":
+        query += "ORDER BY transaction_date ASC"
+    else:
+        query += "ORDER BY transaction_date DESC"
+    
+    df = pd.read_sql_query(query, conn) # executes the query and returns a dataframe
+    st.dataframe(df, hide_index=True) # displays the dataframe in the web app without the index
